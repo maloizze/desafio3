8d3b2211fa42d4b145e5dfc63a5d4729d7395898
Maria Eloise <ml2691098@gmail.com>
2025-06-26 16:54:39 +0000

    desafio26/06

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import './App.css';
+import "./styles/theme.css";
+import "./styles/global.css";
+import { MyTextList } from "./components/MyTextList";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { LuckyNumber } from "./components/etapa2/LuckyNumber";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <LuckyNumber />
+      </div>
+    </>
   );
 }
 
-export default App;
+
